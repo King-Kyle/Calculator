8f262c2b915658f3517c23606a570e180513c939
Kyle <kylek918@icloud.com>
2018-08-27 23:57:33 +0200

    updated

--- js/main.js
+++ js/main.js
@@ -1,82 +1,88 @@
-let result = 0;
+//LINKS TO HTML KEYS
+const equal = document.getElementById('equal');
+const clear = document.getElementById('clear');
+const period = document.getElementById('period');
+const operator = document.querySelectorAll('.operator');
+const digit = document.querySelectorAll(".digit");
+//LINK TO HTML SCREEN
+const display = document.getElementById("display");
+const displayAnswer = document.getElementById("displayAnswer");
+//OPERATOR CHOICES
+const symbol = "+−×÷";
 
-let add = document.getElementById("add");
-let subtract = document.getElementById("subtract");
-let multiply = document.getElementById("multiply");
-let divide = document.getElementById("divide");
+//MATH FUNCTIONS
+let addNum = (num1, num2,) => {return num1 + num2};
+let subtractNum = (num1, num2,) => {return num1 - num2};
+let multiplyNum = (num1, num2,) => {return num1 * num2};
+let divideNum = (num1, num2,) => {return num1 / num2};
 
-let addNum = (a, b) => {return a + b};
-let subtractNum = (a, b) => {return a - b};
-let multiplyNum = (a, b) => {return a * b};
-let divideNum = (a, b) => {return a / b};
-
-function operate(num1, num2, operator){
-  let operator = clicked_id;
-  if(operator == add){
-    return addNum(num1, num2);
-  } else if(operator == subtract){
-    return subtractNum(num1, num2)
-  } else if(operator == multiply){
-    return multiplyNum(num1, num2)
-  } else if(operator == divide){
-    return divideNum(num1, num2)
+//CALLS MATH FUNCTION BASED ON OPERATOR
+function operate(num1, num2, symbols){
+  if(symbol == "+"){
+    return result = addNum(num1, num2);
+  } else if(symbol == "−"){
+    return result = subtractNum(num1, num2)
+  } else if(symbol == "×"){
+    return result = multiplyNum(num1, num2)
+  } else if(symbol == "÷"){
+    return result = divideNum(num1, num2)
+  } else {
+    console.log("ERROR incorrect input")
   }
 }
 
-let displayValue = (clicked_id) =>{
-  let display = document.getElementById("display");
-  let selection = clicked_id;
-  switch(selection){
-    case divide:
-      return display.textContent = "÷";
-      break;
-    case multpily:
-      return display.textContent = "x";
-      break;
-    case add:
-      return display.textContent = "+";
-      break;
-    case subtract:
-      return display.textContent = "-";
-      break;
-    case selection === "one":
-      return display.textContent = "1";
-      break;
-    case selection === "two":
-      return display.textContent = "2";
-      break;
-    case selection === "three":
-      return display.textContent = "3";
-      break;
-    case selection === "four":
-      return display.textContent = "4";
-      break;
-    case selection === "five":
-      return display.textContent = "5";
-      break;
-    case selection === "six":
-      return display.textContent = "6";
-      break;
-    case selection === "seven":
-      return display.textContent = "7";
-      break;
-    case selection === "eight":
-      return display.textContent = "8";
-      break;
-    case selection === "nine":
-      return display.textContent = "9";
-      break;
-    case selection === "zero":
-      return display.textContent = "0";
-      break;
-    case selection === "period":
-      return display.textContent = ",";
-      break;
-    case selection === "clear":
-      return display.textContent = " ";
-      break;
-    case selection === "equal":
-      return display.textContent = "=";
-      break;
-  }
-};
+//INPUTS SELECTED DIGIT ON CLICK
+digit.forEach((number) =>{
+	number.addEventListener('click', (e) => {
+		if (display.textContent == "ERROR"){
+			display.textContent = "";
+		}
+		display.textContent += number.textContent;
+	})
+});
+
+//INPUTS SELECTED OPERATOR ON CLICK
+operator.forEach((operation) =>{
+	operation.addEventListener('click', (e) => {
+		if (display.textContent == "ERROR"){
+			display.textContent = "";
+		}
+		display.textContent += operation.textContent;
+	})
+});
+
+//INPUTS DECIMAL POINT ON CLICK
+period.addEventListener('click', (e) =>{
+	let stringOperations = display.textContent.match(/[^\d()]+|[\d.]+/g);
+	if(stringOperations[stringOperations.length -1].indexOf(".") == -1){
+		display.textContent += ".";
+	}
+});
+
+//COMPLETES THE EQUATION
+equal.addEventListener('click', (e) =>{
+	let stringOperations = display.textContent.match(/[^\d()]+|[\d.]+/g);
+	let product = 0;
+	for (let i = 0; i < stringOperations.length; i++){
+		if(symbol.indexOf(stringOperations[i]) != -1){
+			let op = stringOperations[i];
+			let num1 = +stringOperations[i-1];
+			let num2 = +stringOperations[i+1];
+			if(op == "/" && num2 == 0){
+				product = "ERROR";
+				break;
+			}
+			product += operate(op, num1, num2);
+		}
+	}
+	//incase of = before finishing operation
+	if(stringOperations.length < 3){
+		product = display.textContent;
+	}
+	display.textContent = product;
+});
+
+//CLEARS FIELD
+clear.addEventListener('click', (e) =>{
+	display.textContent = "";
+});
