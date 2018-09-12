0e0810f906c329a222aeb9b7169253dc5cbbc3fe
Kyle <kylek918@icloud.com>
2018-09-12 21:13:17 +0200

    updated calc

--- js/main.js
+++ js/main.js
@@ -1,88 +1,114 @@
-//LINKS TO HTML KEYS
+//LINKS TO HTML BUTTONS
 const equal = document.getElementById('equal');
-const clear = document.getElementById('clear');
-const period = document.getElementById('period');
-const operator = document.querySelectorAll('.operator');
-const digit = document.querySelectorAll(".digit");
-//LINK TO HTML SCREEN
-const display = document.getElementById("display");
-const displayAnswer = document.getElementById("displayAnswer");
-//OPERATOR CHOICES
-const symbol = "+−×÷";
+equal.addEventListener('click', operate);
 
-//MATH FUNCTIONS
-let addNum = (num1, num2,) => {return num1 + num2};
-let subtractNum = (num1, num2,) => {return num1 - num2};
-let multiplyNum = (num1, num2,) => {return num1 * num2};
-let divideNum = (num1, num2,) => {return num1 / num2};
+const operator = document.getElementsByClassName('operator');
+for (let i = 0; i < operator.length; i++) {
+    let operationButton = operator[i];
+    operationButton.addEventListener('click', assignOperator);
+}
+
+const digit = document.getElementsByClassName('digit');
+for (let j = 0; j < digit.length; j++) {
+    let numberBtn = digit[j];
+    numberBtn.addEventListener('click', displayNumber)
+}
+
+const clear = document.getElementById('clear');
+clear.addEventListener('click', clearAll);
+
+//LINKS TO HTML SCREEN
+const display = document.getElementById('display');
+const displayAnswer = document.getElementById('displayAnswer');
+
+//CALCULATION GLOBALS
+let value = 0;
+let num1 = 0
+let num2 = 0
+let operation = '';
+let didOperate = false;
 
 //CALLS MATH FUNCTION BASED ON OPERATOR
-function operate(num1, num2, symbols){
-  if(symbol == "+"){
-    return result = addNum(num1, num2);
-  } else if(symbol == "−"){
-    return result = subtractNum(num1, num2)
-  } else if(symbol == "×"){
-    return result = multiplyNum(num1, num2)
-  } else if(symbol == "÷"){
-    return result = divideNum(num1, num2)
-  } else {
-    console.log("ERROR incorrect input")
-  }
+function operate() {
+    if (operation === '+') {
+       value = num1 + num2;
+    } else if (operation === '-') {
+        value = num1 - num2;
+    } else if (operation === '*') {
+        value = num1 * num2;
+    } else {
+        value = num1 / num2;
+    }
+    display.textContent = num1 + operation + num2;
+    displayAnswer.textContent = value;
+    num1 = value;
+    num2 = 0;
+    didOperate = false;
+    operation = '';
+    console.log({ num1, num2, operation, value });
 }
 
 //INPUTS SELECTED DIGIT ON CLICK
-digit.forEach((number) =>{
-	number.addEventListener('click', (e) => {
-		if (display.textContent == "ERROR"){
-			display.textContent = "";
-		}
-		display.textContent += number.textContent;
-	})
-});
+function displayNumber() {
+    if (value == 0){
+        value = this.innerHTML;
+    } else {
+        value = value + this.innerHTML;
+    }
 
-//INPUTS SELECTED OPERATOR ON CLICK
-operator.forEach((operation) =>{
-	operation.addEventListener('click', (e) => {
-		if (display.textContent == "ERROR"){
-			display.textContent = "";
-		}
-		display.textContent += operation.textContent;
-	})
-});
-
-//INPUTS DECIMAL POINT ON CLICK
-period.addEventListener('click', (e) =>{
-	let stringOperations = display.textContent.match(/[^\d()]+|[\d.]+/g);
-	if(stringOperations[stringOperations.length -1].indexOf(".") == -1){
-		display.textContent += ".";
-	}
-});
+    if (didOperate == true) {
+        num2 = parseFloat(value);
+    }
+    // if(displayValue.includes('.')) {
+    //
+    // }
+    display.textContent = value;
+    console.log({ num1, num2, operation, value });
+}
 
 //COMPLETES THE EQUATION
-equal.addEventListener('click', (e) =>{
-	let stringOperations = display.textContent.match(/[^\d()]+|[\d.]+/g);
-	let product = 0;
-	for (let i = 0; i < stringOperations.length; i++){
-		if(symbol.indexOf(stringOperations[i]) != -1){
-			let op = stringOperations[i];
-			let num1 = +stringOperations[i-1];
-			let num2 = +stringOperations[i+1];
-			if(op == "/" && num2 == 0){
-				product = "ERROR";
-				break;
-			}
-			product += operate(op, num1, num2);
-		}
-	}
-	//incase of = before finishing operation
-	if(stringOperations.length < 3){
-		product = display.textContent;
-	}
-	display.textContent = product;
-});
+function assignOperator() {
+    if(didOperate === false) {
+        num1 = parseFloat(value);
+        if (this.innerHTML == '+') {
+            operation = '+';
+        } else if (this.innerHTML == '−') {
+            operation = '-';
+        } else if (this.innerHTML == '×') {
+            operation = '*';
+        } else {
+            operation = '/';
+        }
+        didOperate = true;
+        value = '';
+    } else {
+        num2 = parseFloat(value);
+    }
+    if(num1 && num2 && operation) {
+        operate();
+        num1 = parseFloat(value);
+        if (this.innerHTML == '+') {
+            operation = '+';
+        } else if (this.innerHTML == '−') {
+            operation = '-';
+        } else if (this.innerHTML == '×') {
+            operation = '*';
+        } else {
+            operation = '/';
+        }
+        didOperate = true;
+        value = '';
+    }
+    console.log({ num1, num2, operation, value });
+}
 
 //CLEARS FIELD
-clear.addEventListener('click', (e) =>{
+function clearAll() {
 	display.textContent = "";
-});
+  displayAnswer.textContent = "";
+  value = 0;
+  didOperate = false;
+  operation = '';
+  num1 = 0;
+  num2 = 0;
+}
